f34dfad4de88639ce08c712387793c4c8a48d6af
chiperific <chiperific@gmail.com>
2018-05-22 18:30:00 -0400

    clean out the cobwebs

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
+class Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def google_oauth2
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
@@ -11,9 +12,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
       end
-  end
+    end
 
-  def failure
-    redirect_to root_path
+    def failure
+      redirect_to root_path
+    end
   end
 end
