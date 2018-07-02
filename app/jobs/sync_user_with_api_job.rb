d5cbbf2d6e7a7c80ea581aad4f570d15bc71a324
chiperific <chiperific@gmail.com>
2018-07-01 23:48:40 -0400

    framework for sync_user_with_api_job_spec

--- app/jobs/sync_user_with_api_job.rb
+++ app/jobs/sync_user_with_api_job.rb
@@ -3,7 +3,7 @@
 class SyncUserWithApiJob < ApplicationJob
   def initialize(user_id)
     @user = User.find(user_id)
-    @tasklists_client = TasklistsClient.new(@user)
+    @tlc = TasklistsClient.new(@user)
   end
 
   def enqueue(job)
@@ -23,9 +23,9 @@ class SyncUserWithApiJob < ApplicationJob
     @job.update_columns(message: 'Assessing the situation...')
 
     # pre-fetch and count tasklists from Google and those missing from Google
-    tls_fetch         = @tasklists_client.fetch
-    tls_count         = @tasklists_client.count
-    tls_count_missing = @tasklists_client.not_in_api.count
+    tls_fetch         = @tlc.fetch
+    tls_count         = @tlc.count
+    tls_count_missing = @tlc.not_in_api.count
     tls_ids           = tls_fetch['items'].map { |i| i['id'] }
 
     # pre-fetch and count tasks from Google and those missing from Google
@@ -47,7 +47,7 @@ class SyncUserWithApiJob < ApplicationJob
     @job.update_columns(message: 'Fetching default tasklist...', progress_current: progress)
     sleep 1
 
-    default_tasklist = @tasklists_client.sync_default
+    default_tasklist = @tlc.sync_default
     progress += 1
     @job.update_columns(message: 'Default tasklist done', progress_current: progress)
     sleep 1
@@ -72,7 +72,7 @@ class SyncUserWithApiJob < ApplicationJob
     @job.update_columns(message: 'Fetching properties...')
     sleep 1
 
-    tasklists = @tasklists_client.sync
+    tasklists = @tlc.sync
     progress += tls_count - 1
     @job.update_columns(message: 'Properties done', progress_current: progress)
     sleep 1
@@ -117,31 +117,35 @@ class SyncUserWithApiJob < ApplicationJob
     @job.update_columns(message: 'Looking for missing properties...')
     sleep 1
 
-    missing_tasklists = @tasklists_client.not_in_api
+    missing_tasklists = @tlc.not_in_api
     if missing_tasklists.present?
       msg = 'Found ' + missing_tasklists.count.to_s + ' missing property'.pluralize(missing_tasklists.count)
       @job.update_columns(message: msg)
       sleep 1
 
-      tc.push
-      msg = 'Processed ' + missing_tasklists.count.to_s + ' missing property'.pluralize(missing_tasklists.count)
+      @tlc.push
+      msg = 'Sent ' + missing_tasklists.count.to_s + ' missing property'.pluralize(missing_tasklists.count)
       progress += missing_tasklists.count
       @job.update_columns(message: msg, progress_current: progress)
 
       # process tasks from app
       @job.update_columns(message: 'Sending tasks from missing properties...')
       sleep 1
-      missing_tasklists.each do |t|
+      missing_tasklists.each_with_index do |t, i|
         tc = TasksClient.new(t)
-        count = tc.count
-        msg = 'Collected ' + count.to_s + ' missing task'.pluralize(count)
-        @job.update_columns(message: msg)
-        sleep 1
+        count = tc.not_in_api # t.tasks.visible_to(@user).count instead???
+        if count.positive?
+          msg = 'Collected ' + count.to_s + ' task'.pluralize(count) + ' from ' + i.ordinalize + ' missing property'
+          @job.update_columns(message: msg)
+          sleep 1
 
-        tc.sync
-        msg = 'Sent ' + count.to_s + ' missing task'.pluralize(count)
-        progress += count
-        @job.update_columns(message: msg, progress_current: progress)
+          tc.push
+          msg = 'Sent ' + count.to_s + ' task'.pluralize(count) + ' from ' + i.ordinalize + ' missing property'
+          progress += count
+          @job.update_columns(message: msg, progress_current: progress)
+        else
+          @job.update_columns(message: 'No tasks exist for ' + i.ordinalize + ' missing property')
+        end
         sleep 1
       end
     else
@@ -154,10 +158,6 @@ class SyncUserWithApiJob < ApplicationJob
     sleep 1
     @job.update_columns(message: 'Done!')
     sleep 1
-  end
-
-  def success(job)
-    # job.destroy(:force)
   end
 
   def max_attempts
--- spec/jobs/sync_user_with_api_job_spec.rb
+++ spec/jobs/sync_user_with_api_job_spec.rb
@@ -1,34 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe SyncUserWithApiJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
-  # FROM: user_spec.rb
-  # describe '#sync_with_api' do
-  #   before :each do
-  #     @oauth_user.save
-  #     3.times { FactoryBot.create(:property, creator: @oauth_user) }
-  #   end
 
-  #   pending 'runs in the background'
-  #     # @oauth_user.sync_with_api
-  #     # expect(Delayed::Worker.new.work_off).to eq [1, 0]
+  describe '#perform' do
+    before :each do
+      @user = double(:user)
+      @tlc = TasklistsClient.new(@user)
+      @tasklist = double(:tasklist)
+      @task_user = double(:task_user)
+      @tc = TasksClient.new(@tasklist)
 
-  #   it 'returns false unless oauth_id is present' do
-  #     @user.save
-  #     expect(@user.sync_with_api).to eq false
-  #   end
+      @list_tasklists_json = JSON.parse(file_fixture('list_tasklists_json_spec.json').read)
+      @tasklist_json = FactoryBot.create(:tasklist_json)
+      @default_tasklist_json = FactoryBot.create(:default_tasklist_json)
+      @list_tasks_json = JSON.parse(file_fixture('list_tasks_json_spec.json').read)
+      @task_json = FactoryBot.create(:task_json)
 
-  #   it 'calls the TasklistClient' do
-  #     expect(TasklistsClient).to receive(:sync)
-  #     @oauth_user.sync_with_api
-  #   end
+      allow(@tlc).to receive(:fetch).and_return(@list_tasklists_json)
+      allow(@tlc).to receive(:count).and_return(3)
+      allow(@tlc).to receive(:not_in_api).and_return([1, 2, 3, 4, 5]) # conditional, line 121
+      allow(@tlc).to receive(:sync_default).and_return(@tasklist)
+      allow(@tlc).to receive(:sync).and_return(@tasklist) # could be multiple
+      allow(@tlc).to receive(:push)
 
-  #   it 'calls the TasksClient' do
-  #     TasklistsClient.sync(@oauth_user)
-  #     count = Property.visible_to(@oauth_user).count
+      allow(TasksClient).to receive(:fetch_with_tasklist_gid_and_user).and_return(@list_tasks_json)
+      allow(TasksClient).to receive(:not_in_api_with_tasklist_gid_and_user).and_return(@task_user) # could be multiple
+      allow(TasksClient).to receive(:new).and_return(@tc)
 
-  #     expect(TasksClient).to receive(:sync).exactly(count).times
-  #     @oauth_user.sync_with_api
-  #   end
-  # end
+      allow(@tc).to receive(:count).and_return(8) # conditional, line 61
+      allow(@tc).to receive(:sync)
+      allow(@tc).to receive(:not_in_api).and_return([1, 2, 3, 4, 5]) # conditional, line 101
+      allow(@tc).to receive(:push)
+    end
+
+    context 'while setting the job\'s max value' do
+      pending 'calls tlc.fetch'
+
+      pending 'calls tlc.count'
+
+      pending 'calls count on tlc.not_in_api'
+
+      pending 'calls TasksClient.fetch_with_tasklist_gid_and_user multiple times'
+
+      pending 'calls TasksClient.not_in_api_with_tasklist_gid_and_user multiple times'
+    end
+
+    context 'while handling the default tasklist' do
+    end
+
+    context 'while handling tasks for the the default tasklist' do
+    end
+
+    context 'while handling non-default tasklists' do
+    end
+
+    context 'while handling tasks for the non-default tasklists' do
+    end
+
+    context 'while handling missing tasklists' do
+    end
+
+    context 'while handling tasks for the missing tasklists' do
+    end
+  end
 end
