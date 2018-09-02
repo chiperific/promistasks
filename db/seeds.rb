4f7e817dacbe2fb5afe893e5efd64642143024fe
chiperific <chiperific@gmail.com>
2018-09-02 14:40:19 -0400

    root page loads

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
 
 skills = %w[plumbing HVAC drywall painting electrical cleaning landscaping roofing siding carpentry framing tiling masonry concrete asphalt demolition trim doors furniture insulating countertops cabinets]
 skills.each do |skill|
