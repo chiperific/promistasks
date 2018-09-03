2d4cb8b77984c5de937caec705d4730bd35a79d9
chiperific <chiperific@gmail.com>
2018-09-03 12:07:29 -0400

    create system spec generator

---
+++ lib/generators/system_spec/templates/system_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe '<%= human_string %>', type: :system do
+  context 'when not current_user' do
+    it 'redirects to login page' do
+      expect(page).to redirect_to(new_user_session_path)
+    end
+  end
+
+  context 'when current_user' do
+    context 'is anyone' do
+      before :each do
+        user = create(:user)
+        login_as(user, scope: :user)
+        visit #path
+      end
+
+      pending 'displays something'
+    end
+
+    context 'is client' do
+      before :each do
+        user = create(:client_user)
+        login_as(user, scope: :user)
+        visit #path
+      end
+
+      it 'flashes error message' do
+        expect(page).to have_content('You do not have permission')
+      end
+
+      it 'redirects away' do
+        expect(current_path).not_to eq #path
+      end
+    end
+
+    context 'is staff' do
+      before :each do
+        user = create(:user)
+        login_as(user, scope: :user)
+        visit #path
+      end
+
+      pending 'displays something'
+    end
+
+    context 'is admin' do
+      before :each do
+        user = create(:admin)
+        login_as(user, scope: :user)
+        visit #path
+      end
+
+      pending 'displays something'
+    end
+
+    context 'is volunteer' do
+      before :each do
+        user = create(:volunteer_user)
+        login_as(user, scope: :user)
+        visit #path
+      end
+
+      pending 'displays something'
+    end
+
+    context 'is contractor' do
+      before :each do
+        user = create(:contractor_user)
+        login_as(user, scope: :user)
+        visit #path
+      end
+
+      pending 'displays something'
+    end
+  end
+
+  context 'when #records are present' do
+  end
+
+  context 'when #records are not present' do
+  end
+end
