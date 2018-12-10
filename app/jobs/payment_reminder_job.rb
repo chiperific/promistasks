24141b2527c4e9bb4125626da6ada6313bd00c44
chiperific <chiperific@gmail.com>
2018-12-09 22:03:45 -0500

    playing with delayed_job, fixed vulnerabilities

--- app/jobs/payment_reminder_job.rb
+++ app/jobs/payment_reminder_job.rb
@@ -1,24 +1,40 @@
 # frozen_string_literal: true
 
 class PaymentReminderJob < ApplicationJob
+  require 'pry-remote'
   queue_as :default
 
-  def perform(*args)
-    puts 'Creating user reminders'
-    payments = Payment.due_within(14)
-    users_ary = []
-    users_ary << Organization.first.billing_contact
+  def initialize(*args)
+    @payments = Payment.due_within(14)
+    @users_ary = []
+    @users_ary << Organization.first.billing_contact
 
-    payments_14.each do |pmt|
-      users_ary << pmt.creator
+    @payments.each do |pmt|
+      @users_ary << pmt.creator
     end
+  end
 
-    users_ary.uniq.each do |user|
+  def enqueue(job)
+    job.record     = Organization.first
+    job.identifier = 'payments_reminder_' + Time.now.utc.rfc3339(3)
+    job.message    = 'Initialized'
+  end
+
+  def before(job)
+    @job = job
+  end
+
+  def perform(*args)
+    @job.upate_columns(message: @job.message + '; Creating user reminders')
+
+    @job.update_columns(message: @job.message + '; Sending reminders for ' + @users_ary.count.to_s + ' users')
+    @users_ary.uniq.each do |user|
       assoc_payments = user.id == Organization.first.billing_contact&.id ?
         payments :
         payments.related_to(user)
 
-      UserMailer.payment_reminder(user, assoc_payments).deliver_later if assoc_payments.any?
+      @job.update_columns(message: @job.message + '; ' + user.name + ': ' + assoc_payments.count.to_s + ' pmts')
+      UserMailer.payments_reminder(user, assoc_payments).deliver_later if assoc_payments.any?
     end
   end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -2,16 +2,22 @@
 
 desc "Email Scheduling Daemon"
 task send_reminders: :environment do
-  puts 'Scheduling the reminders to occur at 8am on weekdays'
+  # puts 'Scheduling the reminders to occur at 8am on weekdays'
   # PaymentReminderJob.set(cron: '0 8 * * 1-5').perform_later
 
-  # Every 2 mins, for testing
-  PaymentReminderJob.set(cron: '0-59/2 * * * *').perform_later
+  # Every minute, for testing
+  puts 'Scheduling the reminders to occur every minute for testing'
+  Delayed::Job.enqueue(PaymentReminderJob.new, cron: '0-59 * * * *')
+  # PaymentReminderJob.set().perform_later
   puts 'Done'
 end
 
 task sync_w_api: :environment do
   puts 'Scheduling the sync to occur hourly'
-  SyncUserWithApiJob.set(cron: @hourly).perform_later
+
+  User.oauth.select(:id).each do |user|
+    Delayed::Job.enqueue(SyncUserWithApiJob.new(user.id), cron: @hourly)
+  end
+  # SyncUserWithApiJob.set(cron: @hourly).perform_later
   puts 'Done'
 end
