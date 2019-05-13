e4c25cf56c916f2264fddd5ba9950420f236aad6
chiperific <chiperific@gmail.com>
2019-05-13 13:57:50 -0400

    add https protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "promisetasks_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://pihtasks.familypromise.org' }
+  config.action_mailer.default_url_options = { host: 'pihtasks.familypromise.org', protocol: 'https' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
